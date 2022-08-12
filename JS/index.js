f803a0ad823f2c7b5af3126ade82629578dd3795
simonpine <simonpineda0521@gmail.com>
2022-08-11 19:18:53 -0500

    hice el primer mini desafio

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+let iva = 1.25;
+
+let gasto1 = prompt("ingrese cuanto dinero gasto en los primero 15 dias de este mes");
+let gasto2 = prompt("ingrese cuanto dinero gasto en la segunda mitad de este mes");
+
+let gastot = Number(gasto1) + Number(gasto2);
+
+console.log(gastot)
+
+let gastoim = gastot * iva;
+
+alert("tu gasto final sera de " + gastoim);
