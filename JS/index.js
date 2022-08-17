591931cede4a065fbf7fccd64c5de0e4c4cdeb18
simonpine <simonpineda0521@gmail.com>
2022-08-16 19:03:15 -0500

    cosas de la clase 2

--- JS/index.js
+++ JS/index.js
@@ -5,8 +5,17 @@ let gasto2 = prompt("ingrese cuanto dinero gasto en la segunda mitad de este mes
 
 let gastot = Number(gasto1) + Number(gasto2);
 
-console.log(gastot)
+console.log(gastot);
 
 let gastoim = gastot * iva;
 
 alert("tu gasto final sera de " + gastoim);
+
+const gastoi = gasto1 ==gasto2;
+
+if(gastoi){
+    alert("eres un cerdo");
+}
+else{
+    alert("me caes bien");
+}
