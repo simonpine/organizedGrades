14a51a6fc0f072f9415130a74219909c74d1e4a1
simonpine <simonpineda0521@gmail.com>
2022-08-20 15:17:01 -0500

    termine la segunda idea para el primer entregable

--- JS/index.js
+++ JS/index.js
@@ -66,3 +66,49 @@ if(num){
 else{
     alert("se dijo hola " + vecesHola + " veces")
 }
+/*--------------------------Idea 2--------------------------*/
+
+let nombre = prompt("Ingresa el nombre de tu nueva mascota");
+let nombre1 = !isNaN(nombre);
+let opciones
+let genero;
+console.log(nombre1);
+if(!nombre1){
+    console.log("Nombre de la mascota es " + nombre);
+}
+else{
+    console.log("Nombre no valido");
+}
+while(nombre1){
+    nombre = prompt("Ingresa un nombre valido");
+    nombre1 = !isNaN(nombre);
+    console.log(nombre1);
+}
+console.log("Nombre de la mascota es " + nombre);
+
+do{
+    console.log("1. Mi mascota es hembra \n 2. Mi mascota es macho \n 3. No espesificado \n 4. Aceptar");
+    opciones = prompt("Elige una opcion");
+    switch(opciones){
+        case "1":
+            genero = "es femenino";
+            break;
+        case "2":
+            genero = "es masculino";
+            break;
+        case "3":
+            genero = "no se sabe";
+            break;
+        case "4":
+            if(genero == undefined){
+                genero = "no se sabe";
+            }
+            break;
+        default:
+            console.log('esa opcion no es valida')
+            break;
+    }
+}while((opciones !== "4"))
+console.log(genero)
+
+alert("Listo!! tu mascota fue registrada, se llama " + nombre + " y su genero " + genero)
