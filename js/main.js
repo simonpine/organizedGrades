9482a7c9d17205b27a179001407d2a5d65c7f084
simonpine <simonpineda0521@gmail.com>
2022-09-03 18:34:19 -0500

    Cree las bases para empezar a crear el proyecto

--- js/main.js
+++ js/main.js
@@ -16,46 +16,6 @@ while(not){
 }
 console.log("El numero de notas que se subiran a la pagina son: ", numeroNotas);
 
-<<<<<<< HEAD
-// if(pais == "colombia")[
-//     conver = 4147
-// ]
-// else if(pais == "argentina")[
-//     conver = 135
-// ]
-// else if(pais == "chile")[
-//     conver = 881
-// ]
-// else{
-//     alert("no tengo ni idea de donde sos");
-// }
-// let dinero = Number(prompt("cuanto dinero (en dolares) quieres pasar a tu moneda"));
-// if (dinero > 1000){
-//     alert("no creo que tengas tanto dinero");
-// }
-// else if (dinero < 10){
-//     alert("no seas tan pobre");
-// }
-// else{
-//     let final = (conver * dinero);
-//     alert("el dinero que ingresaste es igual a: " + final + " pesos de " + pais);
-// }
-
-// // strings pro
-// // === tipo de dato igual (tambien)
-// // isNaN == no es un numero
-
-// let num = parseInt(prompt("ingresa un numero1"))
-// for (let i = 0; i <= 10; i++){
-//     let multi = num
-//     console.log(num * i)
-// }
-
-// brake == detener el bucle
-// continue == saltarse ese paso
-// parseInt == convertir un string en Number
-// isNaN es para saber si es diferente a un numero
-=======
 var numeroAprobar= Number(prompt("Con cuanto se apruban las notas?"));
 let na = isNaN(numeroAprobar);
 while(na){
@@ -105,4 +65,3 @@ console.log("Los estudiantes que aprobaron son: ")
 console.table(aprobados)
 console.log("Los estudiantes que no aprobaron son: ")
 console.table(desaprobados)
->>>>>>> desafio3
