4ca0d7225480382058ad0fbae9a7bb591c75de97
simonpine <simonpineda0521@gmail.com>
2022-09-10 13:36:53 -0500

    Termine la tabla, que muestra notas y estudiantes

--- js/buttons.js
+++ js/buttons.js
@@ -1,12 +1,54 @@
 let submit = document.querySelector("#submit");
 let close = document.querySelector("#close");
 let grades = document.querySelector("#inputGrades");
+let tr_th = document.querySelector("#tr_th");
+let tr = document.querySelector("tr");
 // let a = "234"
 // if (true){
 //     let hola = document.createElement("div")
-//     hola.innerHTML = `<h3>hosaldkasd ${a} </h3>`;
+//     hola.innerText = `<h3>hosaldkasd ${a} </h3>`;
 //     grades.append(hola);
 // }
-let h = [];
-h.unshift(grades);
-console.log(h);
+submit.addEventListener('click', () => grades.classList.remove('out'))
+close.addEventListener('click', () => {
+    grades.classList.add('out');
+    tr_th.innerHTML = ``;
+})
+submit.addEventListener('click', get)
+function get(){
+    var canti = []
+    var numeroEstudiantes = document.querySelector("#numeroEstudiantes");
+    var numeroNotas = document.querySelector("#numeroNotas");
+    var numeroAprobar = document.querySelector("#numeroAprobar");
+    canti.push(Number(numeroAprobar.value), Number(numeroEstudiantes.value), Number(numeroNotas.value))
+    for(a in canti){
+        if (canti[a] === ""){
+            canti[a] = 0
+        }
+    }
+    // <input type="text" class="input1" id="nombre${i}" placeholder="Name"></input>
+    console.log(canti);
+    const up = document.createElement("tr")
+    const nombres = document.createElement("tr")
+    up.innerHTML = `<th></th>`;
+    nombres.innerHTML = `<th>Name</th>`;
+    for(let i = 0; i < canti[1]; i++){
+        const a = document.createElement("th")
+        const b = document.createElement("td")
+        a.innerHTML = `Estudent ${i}`
+        b.innerHTML = `<input type="text" class="input1" id="nombre${i}" placeholder="Name"></input>`
+        up.append(a)
+        nombres.append(b)
+        tr_th.append(up, nombres)
+    }
+    for(let i = 0; i < canti[2]; i++){
+        const notas = document.createElement("tr")
+        notas.innerHTML = `<th>Grade ${i}</th>`
+        for(let x = 0; x < canti[1]; x++){
+            const c = document.createElement("td")
+            c.innerHTML = `<input type="number" class="input1" id="nota${i}estudiante${x}" placeholder="Grade"></input>`
+            notas.append(c)
+            tr_th.append(notas)
+        }
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,67 +1,61 @@
-// El proyecto va a continuar por la linea de planilla de notas, pero ahora la cantidad de notas que fueron hechas puede ser escogida.
-//primero pido cuantos estudiantes tiene, Cuantas nota se sacaron y con cuanto se aprueba, y verifico que se digite un numero 
-var numeroEstudiantes= Number();
-let est = isNaN(numeroEstudiantes);
-while(est){
-  numeroEstudiantes= Number(prompt("El dato ingresado no es valido"));
-  est = isNaN(numeroEstudiantes);
-}
-console.log("El numero de estudiantes que tuviste es: ", numeroEstudiantes);
+// // El proyecto va a continuar por la linea de planilla de notas, pero ahora la cantidad de notas que fueron hechas puede ser escogida.
+// //primero pido cuantos estudiantes tiene, Cuantas nota se sacaron y con cuanto se aprueba, y verifico que se digite un numero 
 
-var numeroNotas= Number();
-let not = isNaN(numeroNotas);
-while(not){
-  numeroNotas= Number();
-  not = isNaN(numeroNotas)
-}
-console.log("El numero de notas que se subiran a la pagina son: ", numeroNotas);
+// // var numeroEstudiantes= Number();
+// // let est = isNaN(numeroEstudiantes);
+// // while(est){
+// //   numeroEstudiantes= Number(prompt("El dato ingresado no es valido"));
+// //   est = isNaN(numeroEstudiantes);
+// // }
+// console.log("El numero de estudiantes que tuviste es: ", numeroEstudiantes);
 
-var numeroAprobar= Number();
-let na = isNaN(numeroAprobar);
-while(na){
-  numeroAprobar= Number(prompt("El dato ingresado no es valido"));
-  na = isNaN(numeroAprobar);
-}
-console.log("Para aprobar se necesita que el estudiante tenga una nota superior a: ", numeroAprobar);
-// Creo el array en donde se guardaran  los estudiantes (con sus notas y su nota final)
-var estudiantes = [];
-// Creo la maqueta para hacer a los estudiantes
-class Estudiante{
-    constructor(name){
-      this.nombre = name;
-      this.nota = [] //creo el array para poder meter facilmente las notas y sacar el promedio
-      this.total = 0
-    }
-}
-//creo un bucle que me hace registrar el nombre y las notas de cada estudiante
-for(let x = 0; x < numeroEstudiantes; x++ ){
-    const nombreEstudiente = prompt("Como se llama el estudiante numero " + x)
-    const estudiante = new Estudiante(nombreEstudiente);
-    for(let i = 1; i <= numeroNotas; i++ ){
-        let Nota = Number(prompt("Nota: " + i))
-        let gd = isNaN(Nota)
-        while(gd){
-            Nota = Number(prompt("El dato ingresado no es valido"))
-            gd = isNaN(Nota)
-        }
-    estudiante.nota.unshift(Nota)
-    }
-    estudiantes.push(estudiante)
-}//creo un bucle que entre el total de cada estudiante
-for (persona of estudiantes){//Tambien se podria usar "for (let i = 0, i < estudiantes.lenght, i++)"
-    persona.total = (persona.nota.reduce((acumulador, elemento) => acumulador + elemento, 0)) / numeroNotas
-    if(persona.total > numeroAprobar){
-        console.log("El estudiante " + persona.nombre + " aprobo el semestre")
-    }
-    else{
-        console.log("El estudiante " + persona.nombre + " no aprobo el semestre")
-    }
-}
-console.log("la lista de estudiantes es: ")
-console.table(estudiantes)
-const aprobados = estudiantes.filter((a) => a.total >= numeroAprobar)//filtro a los estudiantes aprobados y desaprobados
-const desaprobados = estudiantes.filter((b) => b.total < numeroAprobar)
-console.log("Los estudiantes que aprobaron son: ")
-console.table(aprobados)
-console.log("Los estudiantes que no aprobaron son: ")
-console.table(desaprobados)
+// // var numeroNotas= Number();
+// // let not = isNaN(numeroNotas);
+// // while(not){
+// //   numeroNotas= Number();
+// //   not = isNaN(numeroNotas)
+// // }
+// console.log("El numero de notas que se subiran a la pagina son: ", numeroNotas);
+
+// // var numeroAprobar= Number();
+// // let na = isNaN(numeroAprobar);
+// // while(na){
+// //   numeroAprobar= Number(prompt("El dato ingresado no es valido"));
+// //   na = isNaN(numeroAprobar);
+// // }
+// console.log("Para aprobar se necesita que el estudiante tenga una nota superior a: ", numeroAprobar);
+// // Creo el array en donde se guardaran  los estudiantes (con sus notas y su nota final)
+//     var estudiantes = [];
+//     class Estudiante{
+//         constructor(name){
+//           this.nombre = name;
+//           this.nota = [] //creo el array para poder meter facilmente las notas y sacar el promedio
+//           this.total = 0
+//         }
+//     }
+//     //creo un bucle que me hace registrar el nombre y las notas de cada estudiante
+//     for(let x = 0; x < numeroEstudiantes.value; x++ ){
+//         const nombreEstudiente = prompt("Como se llama el estudiante numero " + x)
+//         const estudiante = new Estudiante(nombreEstudiente);
+//         for(let i = 1; i <= numeroNotas.value; i++ ){
+//             let Nota = Number(prompt("Nota: " + i))
+//             let gd = isNaN(Nota)
+//             while(gd){
+//                 Nota = Number(prompt("El dato ingresado no es valido"))
+//                 gd = isNaN(Nota)
+//             }
+//         estudiante.nota.unshift(Nota)
+//         }
+//         estudiantes.push(estudiante)
+//     }//creo un bucle que entre el total de cada estudiante
+//     for (persona of estudiantes){//Tambien se podria usar "for (let i = 0, i < estudiantes.lenght, i++)"
+//         persona.total = (persona.nota.reduce((acumulador, elemento) => acumulador + elemento, 0)) / numeroNotas.value
+//     }
+//     console.log("la lista de estudiantes es: ")
+//     console.table(estudiantes)
+//     const aprobados = estudiantes.filter((a) => a.total >= numeroAprobar.value)//filtro a los estudiantes aprobados y desaprobados
+//     const desaprobados = estudiantes.filter((b) => b.total < numeroAprobar.value)
+//     console.log("Los estudiantes que aprobaron son: ")
+//     console.table(aprobados)
+//     console.log("Los estudiantes que no aprobaron son: ")
+//     console.table(desaprobados)
