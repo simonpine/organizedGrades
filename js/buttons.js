a8631f8e36efa5bf2437cf7d359dc06ae498b5db
simonpine <simonpineda0521@gmail.com>
2022-09-22 17:05:32 -0500

    Arregle la tabulacion

--- js/buttons.js
+++ js/buttons.js
@@ -17,20 +17,20 @@ close.addEventListener('click', () => {
     canti = []
 })
 let canti = []
-function initial(){
+function initial() {
     var numeroEstudiantes = document.querySelector("#numeroEstudiantes");
     var numeroNotas = document.querySelector("#numeroNotas");
     var numeroAprobar = document.querySelector("#numeroAprobar");
     canti.push(Number(numeroAprobar.value), Number(numeroEstudiantes.value), Number(numeroNotas.value));
-    for(a in canti){
-        if (canti[a] < 1){
+    for (a in canti) {
+        if (canti[a] < 1) {
             canti[a] = 1
         }
     }
     localStorage.setItem(`aprove`, numeroAprobar.value)
 }
 submit.addEventListener('click', get);//creo el evento cuando hacen click en el submit
-function get(){
+function get() {
     localStorage.clear()
     initial();
     console.log(canti); //lo que hizo de aqui pa atras fue recopilar los datos que habian en las secciones (numero de estudiantes, notas y con cuanto se aprueba) y los metio en 2 arrays
@@ -38,7 +38,7 @@ function get(){
     const nombres = document.createElement("tr"); //agrego la segunda fila, ahi va a ir el nombre del estudiante
     up.innerHTML = `<th></th>`;// pongo el cuadro de la esquina vacio
     nombres.innerHTML = `<th>Name</th>`;
-    for(let i = 0; i < canti[1]; i++){ //bucle que se repite por cada estudiante, este defina la cantidad de columnas que hay
+    for (let i = 0; i < canti[1]; i++) { //bucle que se repite por cada estudiante, este defina la cantidad de columnas que hay
         const a = document.createElement("th"); //los titulos de las filas
         const b = document.createElement("td"); //los inputs, que van a ser nombres
         a.innerHTML = `Estudent ${i}`
@@ -47,10 +47,10 @@ function get(){
         nombres.append(b);
         tr_th.append(up, nombres); //termino de agregar los contenidos dentro de las primeras 2 filas (numero y nombres)
     }
-    for(let i = 0; i < canti[2]; i++){ //bucle de cantidad de notas por estudiante, definira cuantas filas habra
+    for (let i = 0; i < canti[2]; i++) { //bucle de cantidad de notas por estudiante, definira cuantas filas habra
         const notas = document.createElement("tr");
         notas.innerHTML = `<th>Grade ${i}</th>`
-        for(let x = 0; x < canti[1]; x++){
+        for (let x = 0; x < canti[1]; x++) {
             const c = document.createElement("td");
             c.innerHTML = `<input type="number" class="input1" id="nota${i}estudiante${x}" placeholder="Grade"></input>`//aqui lo que hice fue generar un id diferenta para cada nota que se cree, ya que dira que nota es y de que estudiante (facilito bastante para meter todo a un array)
             notas.append(c);
@@ -58,21 +58,21 @@ function get(){
         }
     }
 }
-class estudiante{
-    constructor(name){
-      this.nombre = name;//creo la maqueta para crear a cada estudiante de manera individual
-      this.nota = [] //creo el array para poder meter facilmente las notas y sacar el promedio
-      this.total = 0
+class estudiante {
+    constructor(name) {
+        this.nombre = name;//creo la maqueta para crear a cada estudiante de manera individual
+        this.nota = [] //creo el array para poder meter facilmente las notas y sacar el promedio
+        this.total = 0
     }
 }
 let gG = document.querySelector("#generateGrades");
 gG.addEventListener('click', gradeSheet);//defino que cuando le den al boton "generate grade sheets" meta todos el nombre y cada una de las notas de cada estudiante a un array (estudiantes)
-function gradeSheet(){
+function gradeSheet() {
     initial();
-    for(let i = 0; i < canti[1]; i++){
+    for (let i = 0; i < canti[1]; i++) {
         const studentName = document.querySelector(`#nombre${i}`);
         const estudianteCreado = new estudiante(studentName.value);
-        for(let x = 0; x < canti[2]; x++){
+        for (let x = 0; x < canti[2]; x++) {
             const nota = document.querySelector(`#nota${x}estudiante${i}`);
             estudianteCreado.nota.push(Number(nota.value));
         }
--- js/result.js
+++ js/result.js
@@ -4,21 +4,21 @@ let aprove = localStorage.getItem('aprove'); //tambien me traigo la informacion 
 const insert = document.querySelector('#insert');
 localStorage.removeItem('aprove');
 opener.addEventListener('click', () => { //creo el evento en donde cuando presiono el boton del menu se despliega o se quita dependiendo de su estado
-    if (opener.classList == ('opener openerClose')){
+    if (opener.classList == ('opener openerClose')) {
         opener.classList.remove('openerClose')
     }
-    else{
+    else {
         opener.classList.add('openerClose')
     }
-    if (select.classList == ('open open2')){
+    if (select.classList == ('open open2')) {
         select.classList.remove('open2')
     }
-    else{
+    else {
         select.classList.add('open2')
     }
 })
 var list = []; //creo el array en donde se pondran todos los estudiantes
-for (let i = 0; i < localStorage.length; i++){ //ahora hago que por cada objeto del localStorage el objeto se pase a formato NO JASON y lo agregue a la lista
+for (let i = 0; i < localStorage.length; i++) { //ahora hago que por cada objeto del localStorage el objeto se pase a formato NO JASON y lo agregue a la lista
     let newEstudentJason = localStorage.getItem(`Estudiante${i}`)
     let newEstudent = JSON.parse(newEstudentJason)
     list.push(newEstudent)
@@ -26,8 +26,8 @@ for (let i = 0; i < localStorage.length; i++){ //ahora hago que por cada objeto 
 console.log(list)
 // ------------------------------------------------------------Aqui dividi la creacion de la lista final en 2 partes------------------------------------------------------- //
 // parte 1 aqui se agrega el numero del estudiante y su nombre (esto facilitara los filtros mas adelante)
-function one(up, nombres){
-    const a = document.createElement("th"); 
+function one(up, nombres) {
+    const a = document.createElement("th");
     const b = document.createElement("td");
     a.innerHTML = `Estudent ${list.indexOf(est)}`
     b.innerHTML = `${est.nombre}`
@@ -36,123 +36,123 @@ function one(up, nombres){
     insert.append(up, nombres);
 }
 // parte 2 se agruega cada nota
-function two(x, i, numeroNota){
+function two(x, i, numeroNota) {
     const c = document.createElement("td");
     c.innerHTML = list[x].nota[i]
     numeroNota.append(c);
     insert.append(numeroNota);
 }
 // ------------------------------------------------------------Ahora creo la funcion en la que se muestran todos los estudiantes------------------------------------------------------- //
-function all(){
-const up = document.createElement("tr"); //creo la primera fila (numero del estudiante)
-const nombres = document.createElement("tr"); //creo la segunda fila (nombre del estudiante)
-up.innerHTML = `<th></th>`;
-nombres.innerHTML = `<th>Name</th>`;
-for (est of list){
-    one(up, nombres); //se hace el proceso de poner numero y nombre del estudiante por cada objeto en la lista de estudiantes
-}
-for(let i = 0; i < list[0].nota.length; i++){
-    const numeroNota = document.createElement("tr");
-    numeroNota.innerHTML = `<th>Grade ${i}</th>` //hago que por el numero de notas se cree una nueva fila
-    for(let x = 0; x < list.length; x++){
-        two(x, i, numeroNota); // y se agrega la nota de cada estudiante en esa fila por cada estudiante que este en la lista
+function all() {
+    const up = document.createElement("tr"); //creo la primera fila (numero del estudiante)
+    const nombres = document.createElement("tr"); //creo la segunda fila (nombre del estudiante)
+    up.innerHTML = `<th></th>`;
+    nombres.innerHTML = `<th>Name</th>`;
+    for (est of list) {
+        one(up, nombres); //se hace el proceso de poner numero y nombre del estudiante por cada objeto en la lista de estudiantes
     }
-}
-// ahora creo la ultima fila en la que se muestra el total de cada estudiante
-const down = document.createElement("tr");
-down.innerHTML = `<th>Total</th>`;
-for (est of list){
-    const d = document.createElement("td");
-    d.innerHTML = est.total
-    down.append(d);
+    for (let i = 0; i < list[0].nota.length; i++) {
+        const numeroNota = document.createElement("tr");
+        numeroNota.innerHTML = `<th>Grade ${i}</th>` //hago que por el numero de notas se cree una nueva fila
+        for (let x = 0; x < list.length; x++) {
+            two(x, i, numeroNota); // y se agrega la nota de cada estudiante en esa fila por cada estudiante que este en la lista
+        }
     }
-insert.append(down);
+    // ahora creo la ultima fila en la que se muestra el total de cada estudiante
+    const down = document.createElement("tr");
+    down.innerHTML = `<th>Total</th>`;
+    for (est of list) {
+        const d = document.createElement("td");
+        d.innerHTML = est.total
+        down.append(d);
+    }
+    insert.append(down);
 }
 all()
 // ------------------------------------------------------------Ahora creo la funcion en la que se muestran todos los estudiantes que no aprobaron------------------------------------------------------- //
-function not(){
+function not() {
     const up = document.createElement("tr");
     const nombres = document.createElement("tr");//los procesos son practicamente lo mismo pero se salta a los estudiantes que tubieron un total mayor al numero de aprobar
     up.innerHTML = `<th></th>`;
     nombres.innerHTML = `<th>Name</th>`;
-for (est of list){
-    if(est.total < aprove){
-        one(up, nombres);
-    }
-}
-for(let i = 0; i < list[0].nota.length; i++){
-    const numeroNota = document.createElement("tr");
-    numeroNota.innerHTML = `<th>Grade ${i}</th>`
-
-    for(let x = 0; x < list.length; x++){
-        if(list[x].total < aprove){
-            two(x, i, numeroNota);
+    for (est of list) {
+        if (est.total < aprove) {
+            one(up, nombres);
         }
     }
-}
-const down = document.createElement("tr");
-down.innerHTML = `<th>Total</th>`;
-for (est of list){
-    if(est.total < aprove){
-    const d = document.createElement("td");
-    d.innerHTML = est.total
-    down.append(d);
+    for (let i = 0; i < list[0].nota.length; i++) {
+        const numeroNota = document.createElement("tr");
+        numeroNota.innerHTML = `<th>Grade ${i}</th>`
+
+        for (let x = 0; x < list.length; x++) {
+            if (list[x].total < aprove) {
+                two(x, i, numeroNota);
+            }
+        }
     }
+    const down = document.createElement("tr");
+    down.innerHTML = `<th>Total</th>`;
+    for (est of list) {
+        if (est.total < aprove) {
+            const d = document.createElement("td");
+            d.innerHTML = est.total
+            down.append(d);
+        }
     }
-insert.append(down);
+    insert.append(down);
 }
 // ------------------------------------------------------------Ahora creo la funcion en la que se muestran todos los estudiantes que aprobaron------------------------------------------------------- //
-function yes(){
+function yes() {
     const up = document.createElement("tr");
     const nombres = document.createElement("tr");//se salta a todos los que no aprobaron
     up.innerHTML = `<th></th>`;
     nombres.innerHTML = `<th>Name</th>`;
-for (est of list){
-    if(est.total >= aprove){
-        one(up, nombres);
-    }
-}
-for(let i = 0; i < list[0].nota.length; i++){
-    const numeroNota = document.createElement("tr");
-    numeroNota.innerHTML = `<th>Grade ${i}</th>`
-
-    for(let x = 0; x < list.length; x++){
-        if(list[x].total >= aprove){
-            two(x, i, numeroNota);
+    for (est of list) {
+        if (est.total >= aprove) {
+            one(up, nombres);
         }
     }
-}
-const down = document.createElement("tr");
-down.innerHTML = `<th>Total</th>`;
-for (est of list){
-    if(est.total >= aprove){
-    const d = document.createElement("td");
-    d.innerHTML = est.total
-    down.append(d);
+    for (let i = 0; i < list[0].nota.length; i++) {
+        const numeroNota = document.createElement("tr");
+        numeroNota.innerHTML = `<th>Grade ${i}</th>`
+
+        for (let x = 0; x < list.length; x++) {
+            if (list[x].total >= aprove) {
+                two(x, i, numeroNota);
+            }
+        }
     }
+    const down = document.createElement("tr");
+    down.innerHTML = `<th>Total</th>`;
+    for (est of list) {
+        if (est.total >= aprove) {
+            const d = document.createElement("td");
+            d.innerHTML = est.total
+            down.append(d);
+        }
     }
-insert.append(down);
+    insert.append(down);
 }
 // ------------------------------------------------------------Ahora creo el evento que cuando le doy al boton de filtrar borro la lista que este puesta y crea una nueva dependiendo de que radio esta seleccionado------------------------------------------------------- //
-const submitFil = document.querySelector('#submitFil');        
+const submitFil = document.querySelector('#submitFil');
 const options = document.querySelectorAll('input[name="radio"]');
 submitFil.addEventListener('click', () => {
     insert.innerHTML = ''
     let selected;
     for (const option of options) { // mira cada radio y cuando encuentra el que esta seleccionado le da su valor a "selected" y rompe el ciclo
-        if (option.checked) { 
+        if (option.checked) {
             selected = option.value;
             break;
         }
     }
     console.log(selected)
-    if (selected == 1){
+    if (selected == 1) {
         all();
     }
-    else if(selected == 2){
+    else if (selected == 2) {
         yes();
     }
-    else{
+    else {
         not();
     }
 })
