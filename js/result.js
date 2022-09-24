0a03e2e34bcaf0443ff75c63d8bf3e61f2187dd5
simonpine <simonpineda0521@gmail.com>
2022-09-24 18:45:39 -0500

    Agregue el toastify y tambien el filtro por nombre

--- js/result.js
+++ js/result.js
@@ -2,13 +2,16 @@ let opener = document.querySelector("#fil");//primero me traigo los objetos que 
 let select = document.querySelector("#select");
 let aprove = localStorage.getItem('aprove'); //tambien me traigo la informacion de con cuanto se aprueva desde el localstorage
 const insert = document.querySelector('#insert');
+const nameFilter = document.querySelector('#nameFilter');
 localStorage.removeItem('aprove');
 opener.addEventListener('click', () => { //creo el evento en donde cuando presiono el boton del menu se despliega o se quita dependiendo de su estado
     if (opener.classList == ('opener openerClose')) {
         opener.classList.remove('openerClose')
+        nameFilter.classList.remove('filterInputOpen')
     }
     else {
         opener.classList.add('openerClose')
+        nameFilter.classList.add('filterInputOpen')
     }
     if (select.classList == ('open open2')) {
         select.classList.remove('open2')
@@ -17,7 +20,7 @@ opener.addEventListener('click', () => { //creo el evento en donde cuando presio
         select.classList.add('open2')
     }
 })
-var list = []; //creo el array en donde se pondran todos los estudiantes
+const list = []; //creo el array en donde se pondran todos los estudiantes
 for (let i = 0; i < localStorage.length; i++) { //ahora hago que por cada objeto del localStorage el objeto se pase a formato NO JASON y lo agregue a la lista
     let newEstudentJason = localStorage.getItem(`Estudiante${i}`)
     let newEstudent = JSON.parse(newEstudentJason)
@@ -60,7 +63,7 @@ function all() {
     }
     // ahora creo la ultima fila en la que se muestra el total de cada estudiante
     const down = document.createElement("tr");
-    down.innerHTML = `<th>Total</th>`;
+    down.innerHTML = insert.innerHTML == ""? `<th>Empty</th>`: `<th>Total</th>`;
     for (est of list) {
         const d = document.createElement("td");
         d.innerHTML = est.total
@@ -91,7 +94,7 @@ function not() {
         }
     }
     const down = document.createElement("tr");
-    down.innerHTML = `<th>Total</th>`;
+    down.innerHTML = insert.innerHTML == ""? `<th>Empty</th>`: `<th>Total</th>`;
     for (est of list) {
         if (est.total < aprove) {
             const d = document.createElement("td");
@@ -123,7 +126,7 @@ function yes() {
         }
     }
     const down = document.createElement("tr");
-    down.innerHTML = `<th>Total</th>`;
+    down.innerHTML = insert.innerHTML == ""? `<th>Empty</th>`: `<th>Total</th>`;
     for (est of list) {
         if (est.total >= aprove) {
             const d = document.createElement("td");
@@ -137,6 +140,12 @@ function yes() {
 const submitFil = document.querySelector('#submitFil');
 const options = document.querySelectorAll('input[name="radio"]');
 submitFil.addEventListener('click', () => {
+      Toastify({
+        text: `The template has been leaked to`,
+        gravity: "bottom",
+        positionLeft: false,
+        className: 'tos'
+      }).showToast();
     insert.innerHTML = ''
     let selected;
     for (const option of options) { // mira cada radio y cuando encuentra el que esta seleccionado le da su valor a "selected" y rompe el ciclo
@@ -154,5 +163,44 @@ submitFil.addEventListener('click', () => {
     }
     else {
         not();
+    }
+})
+// ------------------------------------------------------------Ahora creo el filtro para nombres------------------------------------------------------- //
+nameFilter.addEventListener('keyup', () => { 
+    insert.innerHTML = ''
+    const nameFilter = document.querySelector('#nameFilter');
+    if (nameFilter.value != ''){
+        const up = document.createElement("tr");
+        const nombres = document.createElement("tr");//se salta a todos los que no aprobaron
+        up.innerHTML = `<th></th>`;
+        nombres.innerHTML = `<th>Name</th>`;
+        for (est of list) {
+            if (est.nombre.toLowerCase().includes(nameFilter.value.toLowerCase())) {
+                one(up, nombres);
+            }
+        }
+        for (let i = 0; i < list[0].nota.length; i++) {
+            const numeroNota = document.createElement("tr");
+            numeroNota.innerHTML = `<th>Grade ${i}</th>`
+    
+            for (let x = 0; x < list.length; x++) {
+                if (list[x].nombre.toLowerCase().includes(nameFilter.value.toLowerCase())) {
+                    two(x, i, numeroNota);
+                }
+            }
+        }
+        const down = document.createElement("tr");
+        down.innerHTML = insert.innerHTML == ""? `<th>Empty</th>`: `<th>Total</th>`;
+        for (est of list) {
+            if (est.nombre.toLowerCase().includes(nameFilter.value.toLowerCase())) {
+                const d = document.createElement("td");
+                d.innerHTML = est.total
+                down.append(d);
+            }
+        }
+        insert.append(down);
+    }
+    else{
+        all();
     }
 })
